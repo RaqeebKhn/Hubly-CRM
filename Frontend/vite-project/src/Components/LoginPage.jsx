617b9625c2aac4ac9b5c80c1fe2fe65b12c4a35f
RaqeebKhn <joysouls.rk@gmail.com>
2025-05-03 01:48:18 +0530

    try5

--- Frontend/vite-project/src/Components/LoginPage.jsx
+++ Frontend/vite-project/src/Components/LoginPage.jsx
@@ -4,7 +4,7 @@ import loginhero from '../Assets/loginhero.png'
 import hubly from '../Assets/logo.png';
 import { useState } from 'react';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.BACKEND_URL;
 
 export default function LoginPage() { 
   const navigate = useNavigate();
--- Frontend/vite-project/src/Components/SignupPage.jsx
+++ Frontend/vite-project/src/Components/SignupPage.jsx
@@ -4,7 +4,7 @@ import "./SignupPage.css";
 import signupImg from "../Assets/loginhero.png";
 import hubly from '../Assets/logo.png';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.BACKEND_URL;
 
 export default function SignupPage() {
   const navigate = useNavigate();
